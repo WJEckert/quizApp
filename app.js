a464e32adcdf241c8a4c7f4a1a05b7f9f7d2a652
Will <wjeckert15@gmail.com>
2020-11-17 20:14:14 -0600

    adding things to JS file

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+'use strict'
 /**
  * Example store structure
  */
@@ -26,6 +27,7 @@ const store = {
     }
   ],
   quizStarted: false,
+  quizFinished: false,
   questionNumber: 0,
   score: 0
 };
@@ -49,10 +51,62 @@ const store = {
 
 // These functions return HTML templates
 
+function generateStartScreen(){
+  store.quizStarted = true;
+  console.log(store.quizStarted);
+  return `<form id="quizQuestion">
+  <input name="readyOrnot" type="radio" value="yes">
+   <label for="yes">Yes</label>   <br>
+  <input name="readyOrnot" type="radio" value="no">
+   <label for="no">No</label>   <br>
+  <button type="submit">Start Quiz</button>
+  </form>`
+}
+
+function generateQuestion() {
+  return `<form id="quizQuestion">
+  <input name="readyOrnot" type="radio" value="yes">
+   <label for="yes">Maybe</label>   <br>
+  <input name="readyOrnot" type="radio" value="no">
+   <label for="no">Maybe</label>   <br>
+  <button type="submit">Submit Question</button>
+  </form>`
+}
+
 /********** RENDER FUNCTION(S) **********/
 
 // This function conditionally replaces the contents of the <main> tag based on the state of the store
 
+function renderQuiz() {
+  // render the shopping list in the DOM
+  console.log('`renderQuiz` ran');
+  if (store.quizStarted === false){
+    const quizStart = generateStartScreen();
+    $('.quiz').html(quizStart);
+  }
+  else if (store.quizFinished === false){
+    const quizQuestion = generateQuestion();
+    $('.quiz').html(quizQuestion);
+  }
+  // insert that HTML into the DOM
+}
+
 /********** EVENT HANDLER FUNCTIONS **********/
 
 // These functions handle events (submit, click, etc)
+
+function handleSubmit(){
+  $('.quiz').submit(function(event){
+    event.preventDefault();
+    renderQuiz();
+  })
+}
+
+
+function main() {
+  renderQuiz();
+  handleSubmit();
+}
+
+
+$(main);
